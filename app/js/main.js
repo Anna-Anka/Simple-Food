44c61064dbf0c8950363ef4c9f6207182e65534e
Anna-Anka <anjbekrenewa@gmail.com>
2022-04-29 16:54:48 +0300

    added homework 13

--- app/js/main.js
+++ app/js/main.js
@@ -1,5 +1,11 @@
 $(function() {
 
+    $('.reviews__slider').slick({
+        dots: true,
+        prevArrow: '<button class="reviews__arrow reviews__arrow--prev"></button>',
+        nextArrow: '<button class="reviews__arrow reviews__arrow--next"></button>',
+    });
 
     var mixer = mixitup('.popular-categories__items');
+
 });
